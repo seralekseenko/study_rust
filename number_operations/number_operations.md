c7bef9fb4eed12ffa14c2bdc1f3d7c06f486d0c7
seralekseenko <seralekseenko@ukr.net>
2023-11-22 23:05:08 +0200

    finish number operations

---
+++ number_operations/number_operations.md
@@ -0,0 +1,3 @@
+# some operations with numbers
+
+[theory](https://rustlangua.github.io/rustbookua.github.io/ch03-02-data-types.html#%D0%A7%D0%B8%D1%81%D0%BB%D0%BE%D0%B2%D1%96-%D0%BE%D0%BF%D0%B5%D1%80%D0%B0%D1%86%D1%96%D1%97)
---
+++ number_operations/src/main.rs
@@ -0,0 +1,28 @@
+//use std::fmt;
+
+
+fn main() {
+    // addition
+    let sum = 5 + 10;
+    println!("sum = 5 + 10 → {} {} {1}", sum, "ololo");
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("difference = 95.5 - 4.3 → {}", difference);
+
+    // multiplication
+    let product = 4 * 30;
+    println!("product = 4 * 30 → {}", product);
+
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("quotient =  56.7 / 32.2 → {}", quotient);
+    let floored = 2 / 3; // Results in 0
+    println!("floored = 2 / 3 → {}", floored);
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("remainder = 43 % 5 → {}", remainder);
+
+    println!(" {Z} {z} {heart_eyed_cat} ", Z = 'Z', z = 'z', heart_eyed_cat = '😻');
+
+}
