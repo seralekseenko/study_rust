481f41d1ffa69aeed38bac187b9989fe86ba0189
seralekseenko <seralekseenko@ukr.net>
2024-02-18 21:36:22 +0200

    use structures

--- rectangle_area/src/main.rs
+++ rectangle_area/src/main.rs
@@ -1,12 +1,20 @@
+struct Rectangle {
+    width: usize,
+    height: usize,
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     );
 }
 
-fn area(dimensions: (usize, usize)) -> usize {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> usize {
+    rectangle.width * rectangle.height
 }
