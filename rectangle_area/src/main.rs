992b2dd95913e7d2dbc4d21fd6dd36c2975fb708
seralekseenko <seralekseenko@ukr.net>
2024-02-18 21:15:23 +0200

    init rectangle

---
+++ rectangle_area/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+}
+
+fn area(width: usize, height: usize) -> usize {
+    width * height
+}
