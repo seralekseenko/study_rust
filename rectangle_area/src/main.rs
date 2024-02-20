a004077e3d3a0d25f10b769bd3faa767f4f4f1c1
seralekseenko <seralekseenko@ukr.net>
2024-02-20 02:26:06 +0200

    try methods in structs

--- rectangle_area/src/main.rs
+++ rectangle_area/src/main.rs
@@ -4,16 +4,41 @@ struct Rectangle {
     height: usize,
 }
 
+impl Rectangle {
+    fn square(size: usize) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> usize {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
-    let scale: usize = 123;
     let rect1 = Rectangle {
-        width: dbg!(30 + scale),
+        width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    dbg!(&rect1);
-
-    //println!("Rect is: {:#?}", rect1);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    let sq = Rectangle::square(3);
+    dbg!(sq);
 }
 
 //fn area(rectangle: &Rectangle) -> usize {
