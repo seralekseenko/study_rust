1c6eb54d187ae2e0d4cf14ade97a3fa002cc1fb8
seralekseenko <seralekseenko@ukr.net>
2024-02-28 07:26:42 +0200

    init enums

---
+++ enums/src/main.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+fn main() {
+    let four = IpAddrKind::V4(127, 0, 0, 1);
+    let six = IpAddrKind::V6(String::from("::1"));
+
+    route(four);
+    route(six);
+}
+
+fn route(ip_kind: IpAddrKind) {
+    dbg!(ip_kind);
+}
