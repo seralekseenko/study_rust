9c6906586ae260534ed5553ef3591e6a7d8df184
seralekseenko <seralekseenko@ukr.net>
2024-03-19 21:53:51 +0200

    finish match control design

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -19,23 +19,71 @@ impl Message {
     }
 }
 
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+    Unnamed,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+impl Coin {
+    fn value_in_cents(&self) -> u8 {
+        match &self {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                // ← here we define the var name "state" for UsState type
+                // this match automaticaly extract a UsState from Quarter
+                println!("State quarter from {:?}!", state);
+                25
+            }
+        }
+    }
+}
+
+fn use_option_match_plus_1(num: Option<usize>) -> Option<usize> {
+    match num {
+        None => None,
+        Some(value) => Some(value + 1),
+    }
+}
+
 fn main() {
-    let four = IpAddrKind::V4(127, 0, 0, 1);
-    let six = IpAddrKind::V6(String::from("::1"));
+    let five = Some(5);
+    let six = use_option_match_plus_1(five);
+    println!("Six is: {}", six.expect("Six should not be empty!"));
+    let none = use_option_match_plus_1(None);
+    println!("None is none: {}", none.is_none());
 
-    route(four);
-    route(six);
-
-    let message = Message::Write(String::from("hello"));
-    message.call();
-
-    let k = 10;
-    let some = Some(4).unwrap_or_else(|| 2 * k);
-    assert_eq!(some, 4);
-    println!("some: {}", some);
-    let none = None.unwrap_or_else(|| 2 * k);
-    assert_eq!(none, 20);
-    println!("none: {}", none);
+    //let coin_p = Coin::Penny;
+    //println!("value of coin_p is: {}", coin_p.value_in_cents());
+    //let coin_q = Coin::Quarter(UsState::Unnamed);
+    //println!("value of coin_q is: {}", coin_q.value_in_cents());
+    //let four = IpAddrKind::V4(127, 0, 0, 1);
+    //let six = IpAddrKind::V6(String::from("::1"));
+    //
+    //route(four);
+    //route(six);
+    //
+    //let message = Message::Write(String::from("hello"));
+    //message.call();
+    //
+    //let k = 10;
+    //let some = Some(4).unwrap_or_else(|| 2 * k);
+    //assert_eq!(some, 4);
+    //println!("some: {}", some);
+    //let none = None.unwrap_or_else(|| 2 * k);
+    //assert_eq!(none, 20);
+    //println!("none: {}", none);
 }
 
 fn route(ip_kind: IpAddrKind) {
