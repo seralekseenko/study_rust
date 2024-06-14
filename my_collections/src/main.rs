9bd39157013e0537347eb250cc55b70d0697e36c
seralekseenko <seroleksiienko@gmail.com>
2024-06-14 20:08:18 +0300

    finish Strings

--- my_collections/src/main.rs
+++ my_collections/src/main.rs
@@ -1,7 +1,22 @@
 mod my_vectors;
+mod my_strings;
+mod my_hash_maps;
+
 
 fn main() {
-    vectors_test();
+    strings_test();
+    // vectors_test();
+}
+
+fn strings_test() {
+    println!("An empty string: {}", my_strings::create_empty_string());
+    println!("Some string: {}", my_strings::get_string_from_literal("it was string literal"));
+    my_strings::modify_empty_string();
+    my_strings::print_modified_strings();
+    my_strings::use_plus_operator_for_string();
+    my_strings::use_format_macros();
+    my_strings::try_get_symbol_from_string();
+    my_strings::play_with_indexes();
 }
 
 fn vectors_test() {
